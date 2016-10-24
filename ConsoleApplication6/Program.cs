bb342048d602fa10aba7592c3897267e65e81906
KFU-user <KFU-user@N-1511375168>
2016-10-24 17:16:27 +0300

    patterns2

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -10,6 +10,96 @@ namespace ConsoleApplication6
     {
         static void Main(string[] args)
         {
+            AbstractStudent edu = new SchoolStudent();
+            edu.Study();
+
+            //edu = new UniversityStudent();
+            //edu.Study();
+
+           // AbstractStudent pupl = new Pupil();
+            //pupl.Study();
+
         }
     }
+
+    public abstract class AbstractStudent
+    {
+        public void Study()
+        {
+            EducationalProcess();
+        }
+
+        public abstract void EducationalProcess();
+    }
+
+    public class SchoolStudent : AbstractStudent
+    {
+        public override void EducationalProcess()
+        {
+            Console.WriteLine("Study at school");
+        }
+    }
+
+    public class UniversityStudent : AbstractStudent
+    {
+        public override void EducationalProcess()
+        {
+            Console.WriteLine("Study at university");
+        }
+    }
+
+    public class Student
+    {
+        private IStudyStrategy _studyStrategy;
+
+        public Student(IStudyStrategy studyStrategy)
+        {
+            _studyStrategy = studyStrategy;
+        }
+
+        public void Study()
+        {
+            _studyStrategy.Learn();
+        }
+
+        public void ChangeEducation()
+        {
+            _studyStrategy.ChangeEducation();
+        }
+    }
+
+    public interface IStudyStrategy
+    {
+        void Learn();
+
+        void ChangeEducation();
+    }
+    
+    public class SchoolStrategy : IStudyStrategy
+    {
+        public void Learn()
+        {
+            Console.WriteLine("Study at school");
+        }
+
+        public void ChangeEducation()
+        {
+            Console.WriteLine("Study at university");
+        }
+    }
+
+    public class UniversityStrategy : IStudyStrategy
+    {
+        public void Learn()
+        {
+            Console.WriteLine("Study at university");
+        }
+
+        public void ChangeEducation()
+        {
+            Console.WriteLine("Study at school");
+        }
+    }
+
+    
 }
